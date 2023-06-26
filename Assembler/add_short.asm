b9ba95b8a68c04580668723d54952ccf56e73e37
cleverever <60687194+cleverever@users.noreply.github.com>
2023-06-26 11:54:58 -0700

    Fixed using wrong operands

--- Assembler/add_short.asm
+++ Assembler/add_short.asm
@@ -3,9 +3,9 @@
 CL
 CP R1 //R1 = 0
 LI 0b0001 #0
-CP R2 //R3 = 1
+CP R3 //R3 = 1
 LI 0b0010 #0
-CP R3 //R2 = 2
+CP R2 //R2 = 2
 LD R1 //R0 = MEM[0x0000]
 CP R1 //R1 = MEM[0x0000]
 LD R2 //R0 = MEM[0x0002]
@@ -14,12 +14,12 @@ LI 0b0000 #3
 LI 0b0000 #2
 LI 0b0000 #1
 LI 0b0100 #0
-CP R7
+CP R7 //R7 is store address
 LI 0b1111 #3
 LI 0b1111 #2
 LI 0b1110 #1
 LI 0b1101 #0
-CP R6
+CP R6 //R6 is branch address
 
 //Cyclic carry addition
 CL
@@ -35,8 +35,8 @@ CP R1 //R1 = R1 & R2 << 1
 CL
 NND R0
 NND R2 //R0 = ~R2
-NND R3 //R0 = R1 | R2
-NND R4 //R0 = R1 XNOR R2
+NND R4 //R0 = R1 | R2
+NND R5 //R0 = R1 XNOR R2
 NND R0 //R0 = R1 XOR R2
 CP R2 //R2 = R1 XOR R2
 CL
