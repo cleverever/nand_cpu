23d4ccd8ae9393ab0b6c109a5ae3b39bd5089a15
cleverever <60687194+cleverever@users.noreply.github.com>
2023-06-27 13:10:30 -0700

    Improved testing

--- Testbench/program1_tb.sv
+++ Testbench/program1_tb.sv
@@ -12,26 +12,20 @@ nand_cpu DUT
     .halt(done)
 );
 
-logic [15 : 0] sum;
-logic [15 : 0] op0;
-logic [15 : 0] op1;
-
-initial begin
-    clk = 1'b0;
-    $readmemb("add_short.bin", DUT.I_MEM.core);
-    sum = $urandom_range(0, 65535);
-    op0 = $urandom_range(0, 65535);
-    op1 = sum - op0;
+task test
+(
+    logic [15 : 0] op0,
+    logic [15 : 0] op1,
+    logic [15 : 0] sum
+);
     pwr = 1'b0;
-    #20ns;
+    #40ns;
     pwr = 1'b1;
     DUT.D_MEM.core[0] = op0[7 : 0];
     DUT.D_MEM.core[1] = op0[15 : 8];
     DUT.D_MEM.core[2] = op1[7 : 0];
     DUT.D_MEM.core[3] = op1[15 : 8];
-end
-
-always @(posedge done) begin
+    wait(done);
     assert (DUT.D_MEM.core[4] == sum[7 : 0] & DUT.D_MEM.core[5] == sum[15 : 8]) begin
         $display("TEST PASSED");
     end
@@ -39,6 +33,23 @@ always @(posedge done) begin
         $display("TEST FAILED");
         $display("Expected sum: %0b", sum);
         $display("Actual sum:   %0b", {DUT.D_MEM.core[5], DUT.D_MEM.core[4]});
+    end    
+endtask
+
+logic [15 : 0] sum;
+logic [15 : 0] op0;
+logic [15 : 0] op1;
+
+initial begin
+    clk = 1'b0;
+    $readmemb("add_short.bin", DUT.I_MEM.core);
+
+    for(int i = 0; i < 64; i++)
+    begin
+        sum = $urandom_range(0, 65535);
+        op0 = $urandom_range(0, 65535);
+        op1 = sum - op0;
+        test(op0, op1, sum);
     end
     $finish;
 end
---
+++ Testbench/program2_tb.sv
@@ -0,0 +1,65 @@
+module program2_tb();
+
+logic clk;
+logic pwr;
+logic done;
+
+nand_cpu DUT
+(
+    .clk(clk),
+    .n_rst(pwr),
+
+    .halt(done)
+);
+
+task test
+(
+    logic signed [15 : 0] op0,
+    logic signed [15 : 0] op1
+);
+    pwr = 1'b0;
+    #40ns;
+    pwr = 1'b1;
+    DUT.D_MEM.core[0] = op0[7 : 0];
+    DUT.D_MEM.core[1] = op0[15 : 8];
+    DUT.D_MEM.core[2] = op1[7 : 0];
+    DUT.D_MEM.core[3] = op1[15 : 8];
+    wait(done);
+    assert (DUT.D_MEM.core[4][0] == (op0 < op1)) begin
+        $display("TEST PASSED - %d < %d", op0, op1);
+    end
+    else begin
+        $display("TEST FAILED - %d < %d", op0, op1);
+        $display("Expected: %s", (op0 < op1)? "true" : "false");
+        $display("Actual:   %s", (DUT.D_MEM.core[4][0])? "true" : "false");
+    end
+endtask
+
+logic signed [15 : 0] op0;
+logic signed [15 : 0] op1;
+logic equal;
+
+initial begin
+    clk = 1'b0;
+    $readmemb("lessthan_short.bin", DUT.I_MEM.core);
+
+    for(int i = 0; i < 64; i++)
+    begin
+        equal = ($urandom_range(0, 3) == 0);
+        op0 = $urandom_range(0, 65535);
+        if(equal) begin
+            op1 = op0;
+        end
+        else begin
+            op1 = $urandom_range(0, 65535);
+        end
+        test(op0, op1);
+    end
+    $finish;
+end
+
+always begin
+    clk <= ~clk;
+    #10ns;
+end
+endmodule
