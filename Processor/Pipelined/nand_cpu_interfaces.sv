beb55e54117e6a86240f7576c2cbd83b3dfca031
cleverever <60687194+cleverever@users.noreply.github.com>
2023-06-28 14:40:52 -0700

    Added passthrough interface for action cycle

--- Processor/Pipelined/nand_cpu_interfaces.sv
+++ Processor/Pipelined/nand_cpu_interfaces.sv
@@ -17,3 +17,21 @@ modport out
     output valid, reg_write, reg_addr, reg_data, ps_write, ps_data
 );
 endinterface
+
+interface act_pass_ifc;
+logic valid;
+logic mem_access;
+logic reg_write;
+logic [3 : 0] reg_addr;
+logic ps_write;
+logic ps_data;
+
+modport in
+(
+    input valid, reg_write, reg_addr, ps_write, ps_data
+);
+modport out
+(
+    output.p['] valid, reg_write, reg_addr, ps_write, ps_data
+);
+endinterface
