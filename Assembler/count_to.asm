9ba82ecaf4b54d68b657f8653389ccb55d691284
cleverever <60687194+cleverever@users.noreply.github.com>
2023-06-28 14:36:00 -0700

    Added a program to test JRL

---
+++ Assembler/count_to.asm
@@ -0,0 +1,82 @@
+//----------------------------------------------------------------------------------------------------
+//COUNT TO
+//----------------------------------------------------------------------------------------------------
+//Input(s): MEM[0x0000]
+//Register usage: R0-10, R15
+//Output(s): MEM[0x0001] - MEM[MEM[0x0000]]
+CL
+LI 0b0001 #0
+CP R10
+CL
+LD R0
+LS R10
+CP R7
+CL
+CP R1
+EQ R7
+LI 0b0001 #1
+LI 0b0010 #0
+BR R0
+NND R0
+LI 0b1111 #1
+LI 0b0100 #0
+CP R9
+CL //LOOP
+LI 0b0001 #1
+LI 0b1111 #0
+CP R15
+JRL R15 //R0++
+CL
+NND R0
+NND R1
+NND R0
+NE R7
+RS R10
+ST R1
+BR R9 //BR LOOP
+HLT #0
+
+
+
+//----------------------------------------------------------------------------------------------------
+//INCREMENT SUBROUTINE
+//----------------------------------------------------------------------------------------------------
+//Input(s): R1, R15
+//R1: Number to increment
+//R15: Return address
+//Register usage: R0-6, R15
+//Output(s): R1
+//R1: R1 + 1
+CL
+LI 0b0001 #0
+CP R3
+LI 0b0010 #0
+CP R2
+LI 0b1111 #3
+LI 0b1111 #2
+LI 0b1110 #1
+LI 0b1101 #0
+CP R6
+
+CL
+NND R0
+NND R2
+CP R4
+NND R0
+NND R1
+CP R5
+NND R0
+LS R3
+CP R2
+CL
+NND R0
+NND R1
+NND R4
+NND R5
+NND R0
+CP R1
+CL
+NE R2
+BR R6
+
+JRL R15
