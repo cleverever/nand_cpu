1b5afdfa0f7a7a559371416c70c2b41774709f7b
cleverever <60687194+cleverever@users.noreply.github.com>
2023-06-28 21:36:20 -0700

    Added shell script for updating program binaries

---
+++ Testbench/update_binaries.ps1
@@ -0,0 +1,9 @@
+cd ..
+foreach($file in Get-ChildItem Assembler\*.asm)
+{
+    python Assembler\assembler.py $file
+}
+foreach($file in Get-ChildItem Assembler\*.bin)
+{
+    copy -path $file -Destination Testbench
+}
