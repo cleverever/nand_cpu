0a6786123cf5f2e0d0abf155fb94a72ef80a050b
cleverever <60687194+cleverever@users.noreply.github.com>
2023-06-29 08:07:22 -0700

    Port changes

--- Processor/Pipelined/d_cache.sv
+++ Processor/Pipelined/d_cache.sv
@@ -2,19 +2,21 @@
 
 interface d_cache_input_ifc;
 logic valid;
+logic [15 : 0] address;
+nand_cpu_pkg::MEM_OP mem_op;
 logic [15 : 0] data;
 
 modport in
 (
-    input valid, data
+    input valid, address, mem_op, data
 );
 modport out
 (
-    output valid, data
+    output valid, address, mem_op, data
 );
 endinterface
 
-interface d_cache_ifc;
+interface d_cache_output_ifc;
 logic valid;
 logic [15 : 0] data;
 
@@ -33,10 +35,9 @@ module d_cache
     input logic clk,
     input logic n_rst,
 
-    decoder_output_ifc.d_mem i_decoder,
-    regfile_output_ifc.d_mem i_regfile,
+    d_cache_input_ifc.in in,
 
-    d_mem_ifc.out out
+    d_cache_output_ifc.out out
 );
 
 logic [7 : 0] core [(2 ** 16) - 1];
@@ -46,17 +47,9 @@ always_ff @(posedge clk) begin
         core <= {default:'0};
     end
     else begin
-        if(i_decoder.valid & i_decoder.mem_access & i_decoder.mem_op == WRITE) begin
-            core[i_regfile.rt + 1] = i_regfile.ra[15 : 8];
-            core[i_regfile.rt] = i_regfile.ra[7 : 0];
-        end
     end
 end
 
 always_comb begin
-    if(i_decoder.valid & i_decoder.mem_access & i_decoder.mem_op == READ) begin
-        data[15 : 8] = core[i_regfile.rt + 1];
-        data[7 : 0] = core[i_regfile.rt];
-    end
 end
 endmodule
