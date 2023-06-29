5201b1f40644fbd006adc7e8edd3f91277243e44
cleverever <60687194+cleverever@users.noreply.github.com>
2023-06-29 08:15:47 -0700

    Minor changes

--- Testbench/update_binaries.ps1
+++ Testbench/update_binaries.ps1
@@ -1,9 +1,9 @@
 cd ..
-foreach($file in Get-ChildItem Assembler\*.asm)
+foreach($file in Get-ChildItem Assembler/*.asm)
 {
-    python Assembler\assembler.py $file
+    python Assembler/assembler.py $file
 }
-foreach($file in Get-ChildItem Assembler\*.bin)
+foreach($file in Get-ChildItem Assembler/*.bin)
 {
     copy -path $file -Destination Testbench
 }
