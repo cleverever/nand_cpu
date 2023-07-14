d32021696ebe164fc4058900cf09df9be45479f1
cleverever <60687194+cleverever@users.noreply.github.com>
2023-07-13 22:20:39 -0700

    Updated test bench for cache memory

--- Testbench/program3_tb.sv
+++ Testbench/program3_tb.sv
@@ -1,8 +1,16 @@
+`include "nand_cpu.svh"
+
 module program3_tb();
 
 logic clk;
 logic pwr;
 logic done;
+
+localparam MEM_WIDTH = (`PC_SIZE - 1 > 16)? `PC_SIZE : 17;
+localparam INSTR_OFFSET = 0;
+localparam BYTES = (2 ** (MEM_WIDTH + 1));
+localparam DATA_OFFSET = 2 ** (MEM_WIDTH - $clog2(`CACHE_BLOCK_SIZE / 16) - 1);
+logic [7 : 0] machine_code [BYTES / 2];
 
 nand_cpu DUT
 (
@@ -12,7 +20,13 @@ nand_cpu DUT
     .halt(done)
 );
 
+logic [7 : 0] core_b [BYTES];
+assign core_b = {>>{DUT.MEMORY.core}};
+
 logic correct;
+logic [DUT.D_CACHE.OFFSET_BITS - 1 : 0] offset;
+logic [DUT.D_CACHE.INDEX_BITS - 1 : 0] index;
+logic [DUT.D_CACHE.TAG_BITS - 1 : 0] tag;
 
 task test
 (
@@ -21,11 +35,17 @@ task test
     pwr = 1'b0;
     #40ns;
     pwr = 1'b1;
-    DUT.D_MEM.core[0] = op0;
+    DUT.MEMORY.core[DATA_OFFSET][15 : 0] = op0;
     wait(done);
     correct = 1'b1;
     for(int i = 1; i < op0 + 1; i++) begin
-        correct = correct & (DUT.D_MEM.core[i] == i[15 : 0]);
+        offset = i[DUT.D_CACHE.OFFSET_BITS - 1 : 0];
+        index = i[DUT.D_CACHE.INDEX_BITS + DUT.D_CACHE.OFFSET_BITS - 1 : DUT.D_CACHE.OFFSET_BITS];
+        tag = i[DUT.D_CACHE.TAG_BITS + DUT.D_CACHE.INDEX_BITS + DUT.D_CACHE.OFFSET_BITS - 1 : DUT.D_CACHE.INDEX_BITS + DUT.D_CACHE.OFFSET_BITS];
+        correct = correct & ((DUT.MEMORY.core[i / (16 / `CACHE_BLOCK_SIZE)][(i % (16 / `CACHE_BLOCK_SIZE)) * 16 +: 16] == i[15 : 0]) |
+        (DUT.D_CACHE.lines[index].valid &
+        DUT.D_CACHE.lines[index].tag == tag &
+        DUT.D_CACHE.lines[index].data[offset * 16 +: 16] == i[15 : 0]));
     end
     assert (correct) begin
         $display("TEST PASSED - Count to %d", op0);
@@ -37,7 +57,10 @@ endtask
 
 initial begin
     clk = 1'b0;
-    $readmemb("../../Testbench/count_to.bin", DUT.I_MEM.core);
+    machine_code = '{default:'0};
+    $readmemb("../../Testbench/count_to.bin", machine_code);
+    DUT.MEMORY.core = {>>`CACHE_BLOCK_SIZE{machine_code}};
+
     for(int i = 0; i < 10; i++)
     begin
         test(i[15 : 0]);
