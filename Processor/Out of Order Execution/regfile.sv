befe091983ace567679f662cfa3d08cf47817f19
cleverever <60687194+cleverever@users.noreply.github.com>
2023-07-15 23:38:39 -0700

    Increased physical registers

--- Processor/Out of Order Execution/regfile.sv
+++ Processor/Out of Order Execution/regfile.sv
@@ -1,15 +1,17 @@
+`include "nand_cpu.svh"
+
 interface regfile_output_ifc;
-logic [`DATA_WIDTH - 1 : 0] ra;
-logic [`DATA_WIDTH - 1 : 0] rt;
+logic [`DATA_WIDTH - 1 : 0] r0;
+logic [`DATA_WIDTH - 1 : 0] r1;
 logic ps;
 
-modport regfile
+modport in
 (
-    output ra, rt, ps
+    input r0, r1, ps
 );
-modport alu
+modport out
 (
-    input ra, rt
+    output r0, r1, ps
 );
 endinterface
 
@@ -18,41 +20,59 @@ module regfile
     input logic clk,
     input logic n_rst,
 
-    writeback_ifc.in i_writeback,
-    decoder_output_ifc.regfile i_reg_read,
+    input logic read_valid,
+
+    input logic use_r0,
+    input logic [$clog2(`NUM_REG) - 1 : 0] r0_addr,
+
+    input logic use_r1,
+    input logic [$clog2(`NUM_REG) - 1 : 0] r1_addr,
+
+    input logic ps_read,
+    input logic [$clog2(`NUM_PS) - 1 : 0] ps_addr,
+
+    input logic write_valid,
+
+    input logic use_rw,
+    input logic [$clog2(`NUM_REG) - 1 : 0] rw_addr,
+    input logic [`DATA_WIDTH - 1 : 0] rw_data,
+
+    input logic ps_write,
+    input logic [$clog2(`NUM_PS) - 1 : 0] ps_addr,
+    input logic ps_data,
 
     regfile_output_ifc.regfile out
 );
 
-logic [`DATA_WIDTH - 1 : 0] regs [16];
-logic ps_reg;
+logic [`DATA_WIDTH - 1 : 0] reg_list [`NUM_REG];
+logic ps_list [`NUM_PS];
 
 always_comb begin
-    if(i_reg_read.valid) begin
-        if(i_reg_read.use_ra) begin
-            out.ra = regs[0];
+    if(valid) begin
+        if(use_r0) begin
+            out.r0 = reg_list[r0_addr];
         end
-        if(i_reg_read.use_rt) begin
-            out.rt = regs[i_reg_read.rt_addr];
+        if(use_r1) begin
+            out.r1 = reg_list[r1_addr];
         end
-        if(i_reg_read.read_ps) begin
-            out.ps = ps_reg;
+        if(ps_read) begin
+            out.ps = ps_list[ps_addr];
         end
     end
 end
 
 always_ff @(posedge clk) begin
     if(~n_rst) begin
-        regs <= '{default:'0};
-        ps_reg <= 1'b0;
+        reg_list <= '{default:'0};
+        ps_list <= '{default:'0};
     end
     else begin
-        if(i_writeback.valid) begin
-            if(i_writeback.use_rw) begin
-                regs[i_writeback.rw_addr] <= i_writeback.data;
+        if(write_valid) begin
+            if(use_rw) begin
+                reg_list[rw_addr] <= rw_data;
             end
             if(i_writeback.write_ps) begin
-                ps_reg <= i_writeback.ps;
+                ps_list[ps_addr] <= ps_data;
             end
         end
     end
