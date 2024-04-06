c1c5ef7a544abce0ac4863ae991c0496e3fc51cb
cleverever <60687194+cleverever@users.noreply.github.com>
2024-04-06 00:21:26 -0700

    Increased readability

--- Processor/Out of Order Execution/alu.sv
+++ Processor/Out of Order Execution/alu.sv
@@ -1,8 +1,8 @@
 `include "nand_cpu.svh"
 
 interface alu_input_ifc;
-logic [15 : 0] op0;
-logic [15 : 0] op1;
+logic [15:0] op0;
+logic [15:0] op1;
 nand_cpu_pkg::AluOp alu_op;
 
 modport in
@@ -19,7 +19,7 @@ module alu
 (
     alu_input_ifc.in in,
 
-    output logic [15 : 0] out
+    output logic [15:0] out
 );
 
 always_comb begin
@@ -40,26 +40,26 @@ always_comb begin
             out = in.op0 >> in.op1;
         end
         ALU_EQ : begin
-            out[15 : 1] = '0;
+            out[15:1] = '0;
             out[0] = in.op0 == in.op1;
         end
         ALU_NE : begin
-            out[15 : 1] = '0;
+            out[15:1] = '0;
             out[0] = in.op0 != in.op1;
         end
         ALU_LI : begin
             case(in.op1[5 : 4])
                 2'b00 : begin
-                    out = {in.op0[15 : 4], in.op1[3 : 0]};
+                    out = {in.op0[15:4], in.op1[3:0]};
                 end
                 2'b01 : begin
-                    out = {in.op0[15 : 8], in.op1[3 : 0], in.op0[3 : 0]};
+                    out = {in.op0[15:8], in.op1[3:0], in.op0[3:0]};
                 end
                 2'b10 : begin
-                    out = {in.op0[15 : 12], in.op1[3 : 0], in.op0[7 : 0]};
+                    out = {in.op0[15:12], in.op1[3:0], in.op0[7:0]};
                 end
                 2'b11 : begin
-                    out = {in.op1[3 : 0], in.op0[11 : 0]};
+                    out = {in.op1[3:0], in.op0[11:0]};
                 end
             endcase
         end
