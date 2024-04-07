c330c8b51d2f4a9ebb7017f5e6e0bb0494b33348
cleverever <60687194+cleverever@users.noreply.github.com>
2024-04-07 16:49:31 -0700

    Added ports to interfaces

--- Processor/Out of Order Execution/free_reg_list.sv
+++ Processor/Out of Order Execution/free_reg_list.sv
@@ -1,30 +1,16 @@
 `include "nand_cpu.svh"
 
 interface free_reg_list_ifc;
-logic clk;
-logic n_rst;
+logic [$clog2(`NUM_D_REG)-1:0] rw_addr;
+logic [$clog2(`NUM_S_REG)-1:0] rs_addr;
 
-logic r_checkin;
-logic [$clog2(`NUM_D_REG)-1 : 0] r_in;
-
-logic r_checkout;
-logic [$clog2(`NUM_D_REG)-1 : 0] r_out;
-
-logic s_checkin;
-logic [$clog2(`NUM_S_REG)-1 : 0] s_in;
-
-logic s_checkout;
-logic [$clog2(`NUM_S_REG)-1 : 0] s_out;
-
-modport self
+modport in
 (
-    input r_checkin, r_in, r_checkout, s_checkin, s_in, s_checkout,
-    output r_out, s_out
+    input rw_addr, rs_addr
 );
-modport other
+modport out
 (
-    input r_out, s_out,
-    output r_checkin, r_in, r_checkout, s_checkin, s_in, s_checkout
+    output rw_addr, rs_addr
 );
 endinterface
 
@@ -33,45 +19,73 @@ module free_reg_list
     input logic clk,
     input logic n_rst,
 
-    free_reg_list_ifc.self port
+    input logic valid,
+    decoder_ifc.in decoder_in,
+
+    reorder_buffer_ifc.in commit,
+    free_reg_list_ifc.out frl_out,
+
+    output logic stall
 );
+
+logic rw_available;
+logic rs_available;
+
+logic checkin_rw;
+logic checkin_rs;
+
+logic checkout_rw;
+logic checkout_rs;
 
 typedef enum {REG_IDLE, REG_BUSY} REG_STATE;
 REG_STATE r_list [`NUM_D_REG];
 REG_STATE s_list [`NUM_S_REG];
 
-logic [$clog2(`NUM_D_REG)-1 : 0] r_free;
-logic [$clog2(`NUM_S_REG)-1 : 0] s_free;
+logic r_free_valid;
+logic [$clog2(`NUM_D_REG)-1:0] r_free;
+
+logic s_free_valid;
+logic [$clog2(`NUM_D_REG)-1:0] s_free;
 
 always_comb begin
+    checkin_rw = commit.valid & commit.use_rw;
+    checkin_rs = commit.valid & commit.use_rs;
+    checkout_rw = valid & decoder_in.use_rw;
+    checkout_rs = valid & decoder_in.use_rs;
+    stall = (checkout_rw & ~rw_available) | (checkout_rs & ~rs_available);
+
+    r_free_valid = 1'b0;
     for(int i = `NUM_D_REG - 1; i >= 0; i--) begin
         if(r_list[i] == REG_IDLE) begin
+            r_free_valid = 1'b1;
             r_free = i;
         end
     end
 
+    s_free_valid = 1'b0;
     for(int i = `NUM_S_REG - 1; i >= 0; i--) begin
         if(s_list[i] == REG_IDLE) begin
+            s_free_valid = 1'b1;
             s_free = i;
         end
     end
 
-    if(port.r_checkout) begin
-        if(port.r_checkin) begin
-            port.r_out = port.r_in;
-        end
-        else begin
-            port.r_out = port.r_free;
-        end
+    if(checkin_rw) begin
+        rw_available = 1'b1;
+        frl_out.rw_addr = commit.rw_addr;
+    end
+    else begin
+        rw_available = r_free_valid;
+        frl_out.rw_addr = r_free;
     end
 
-    if(port.s_checkout) begin
-        if(port.s_checkin) begin
-            port.s_out = port.s_in;
-        end
-        else begin
-            port.s_out = port.s_free;
-        end
+    if(checkin_rs) begin
+        rs_available = 1'b1;
+        frl_out.rs_addr = commit.rs_addr;
+    end
+    else begin
+        rs_available = s_free_valid;
+        frl_out.rs_addr = s_free;
     end
 end
 
@@ -86,18 +100,18 @@ always_ff @(posedge clk) begin
         end
     end
     else begin
-        case({port.r_checkin, port.r_checkout})
+        case({checkin_rw, checkout_rw})
             2'b10 : begin
-                r_list[r_in] <= REG_IDLE;
+                r_list[commit.rw_addr] <= REG_IDLE;
             end
             2'b01 : begin
                 r_list[r_out] <= REG_BUSY;
             end
         endcase
 
-        case({port.s_checkin, port.s_checkout})
+        case({checkin_rs, checkout_rs})
             2'b10 : begin
-                s_list[s_in] <= REG_IDLE;
+                s_list[commit.rs_addr] <= REG_IDLE;
             end
             2'b01 : begin
                 s_list[s_out] <= REG_BUSY;
