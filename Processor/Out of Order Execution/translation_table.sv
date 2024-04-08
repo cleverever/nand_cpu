f59bf3190164ae990ee9032e70cf732d850e89aa
cleverever <60687194+cleverever@users.noreply.github.com>
2024-04-07 18:28:00 -0700

    Update translation_table.sv

--- Processor/Out of Order Execution/translation_table.sv
+++ Processor/Out of Order Execution/translation_table.sv
@@ -5,17 +5,19 @@ logic [$clog2(`NUM_D_REG)-1:0] p_ra_addr;
 logic [3:0] v_ra_addr;
 logic [$clog2(`NUM_D_REG)-1:0] p_rt_addr;
 logic [3:0] v_rt_addr;
+logic [$clog2(`NUM_D_REG)-1:0] p_rw_addr;
+logic [3:0] v_rw_addr;
 logic [$clog2(`NUM_S_REG)-1:0] p_rs_addr;
 
 modport tt
 (
-    input v_ra_addr, v_rt_addr,
-    output p_ra_addr, p_rt_addr, p_rs_addr
+    input v_ra_addr, v_rt_addr, v_rw_addr
+    output p_ra_addr, p_rt_addr, p_rw_addr, p_rs_addr
 );
 modport other
 (
-    input p_ra_addr, p_rt_addr, p_rs_addr,
-    output v_ra_addr, v_rt_addr
+    input p_ra_addr, p_rt_addr, p_rw_addr, p_rs_addr,
+    output v_ra_addr, v_rt_addr, v_rw_addr
 );
 endinterface
 
@@ -37,6 +39,7 @@ logic [$clog2(`NUM_S_REG)-1:0] s_translation;
 always_comb begin
     port.p_ra_addr = d_translation[port.v_ra_addr];
     port.p_rt_addr = d_translation[port.v_rt_addr];
+    port.p_rw_addr = d_translation[port.v_rw_addr];
     port.p_rs_addr = s_translation;
 end
 
