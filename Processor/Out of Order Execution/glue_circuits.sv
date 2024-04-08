d8939c0eed5ce94fe393e2a88a7dc3399c8136d5
cleverever <60687194+cleverever@users.noreply.github.com>
2024-04-07 18:27:43 -0700

    Update glue_circuits.sv

--- Processor/Out of Order Execution/glue_circuits.sv
+++ Processor/Out of Order Execution/glue_circuits.sv
@@ -1,86 +1,33 @@
 `include "nand_cpu.svh"
 
-interface decode_glue_ifc;
-logic use_ra;
-logic [$clog2(`NUM_D_REG)-1 : 0] ra_addr
-
-logic use_rt;
-logic [$clog2(`NUM_D_REG)-1 : 0] rt_addr;
-
-logic use_rw;
-logic [$clog2(`NUM_D_REG)-1 : 0] rw_addr;
-
-logic uses_rs;
-logic [$clog2(`NUM_S_REG)-1 : 0] rs_addr;
-
-logic use_immdt;
-logic [3 : 0] immdt;
-logic [5 : 4] shift;
-
-logic mem_access;
-nand_cpu_pkg::MemOp mem_op;
-
-logic jump;
-logic branch;
-
-logic interrupt;
-logic halt;
-
-nand_cpu_pkg::AluOp alu_op;
-
-modport in
-(
-    input use_ra, ra_addr, use_rt, rt_addr, use_rw, rw_addr, use_rs, rs_addr,
-    use_immdt, immdt, shift, mem_access, mem_op, jump, branch, interrupt, halt, alu_op
-);
-modport out
-(
-    output use_ra, ra_addr, use_rt, rt_addr, use_rw, rw_addr, use_rs, rs_addr,
-    use_immdt, immdt, shift, mem_access, mem_op, jump, branch, interrupt, halt, alu_op
-);
-endinterface
-
 module decode_glue
 (
-    decoder_ifc.other dec,
+    input logic valid,
+    decoder_ifc.in decoder_in,
     translation_table_ifc.other tt,
-    free_reg_list_ifc.other rfl,
+    free_reg_list_ifc.other frl,
+    input logic [$clog2(`ROB_SIZE)-1:0] rob_addr,
 
-    decode_glue_ifc.out out
+    execution_buffer_ifc.out eb_out
 );
 
 always_comb begin
-    out.use_ra = dec.use_ra;
-    out.ra_addr = tt.d_translation[0];
+    tt.v_ra_addr = decoder_in.ra_addr;
+    tt.v_rt_addr = decoder_in.rt_addr;
+    tt.v_rw_addr = decoder_in.rw_addr;
 
-    out.use_rt = dec.use_rt;
-    out.rt_addr = tt.d_translation[d.rt_addr];
-
-    out.use_rw = dec.use_rw;
-    out.rw_addr = frl.r_out;
-
-    out.uses_rs = dec.use_rs;
-    out.rs_addr = frl.s_out;
-
-    out.use_immdt = dec.use_immdt;
-    out.immdt = dec.immdt;
-    out.shift = dec.shift;
-
-    out.mem_access = dec.mem_access;
-    out.mem_op = dec.mem_op;
-
-    out.jump = dec.jump;
-    out.branch = dec.branch;
-
-    out.interrupt = dec.interrupt;
-    out.halt = dec.halt;
-
-
-    tt.d_set = d.use_rw;
-    tt.d_v_reg = d.rw_addr;
-    tt.d_p_reg = frl.out;
-
-    frl.checkout = d.use_rw;
+    eb_out.valid = valid;
+    eb_out.rob_addr = rob_addr;
+    eb_out.alu_op = decoder_in.alu_op;
+    eb_out.immdt = decoder_in.immdt;
+    eb_out.ra_addr = tt.p_ra_addr;
+    eb_out.use_rt = decoder_in.use_rt;
+    eb_out.rt_addr = tt.p_rt_addr;
+    eb_out.write_dst = decoder_in.use_rs;
+    eb_out.rw_addr = frl.rw_addr;
+    eb_out.prev_rw_addr = tt.p_rw_addr;
+    eb_out.rs_addr = frl.rs_addr;
+    eb_out.prev_rs_addr = tt.p_rs_addr;
 end
 endmodule
 
