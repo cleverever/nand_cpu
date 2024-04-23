7a51bfb5489aeb126ea215636e3fc8e9704e2dbd
cleverever <60687194+cleverever@users.noreply.github.com>
2024-04-22 23:40:16 -0700

    Added macros for buffer length

--- Processor/Out of Order Execution/nand_cpu.svh
+++ Processor/Out of Order Execution/nand_cpu.svh
@@ -6,6 +6,11 @@
 `define CACHE_BLOCK_SIZE 256
 `define MEM_TRANS_SIZE 64
 
+`define EX_B_LENGTH 16
+`define BR_B_LENGTH 4
+`define ST_B_LENGTH 8
+`define LD_B_LENGTH 8
+
 `define ROB_LENGTH 64
 
 import nand_cpu_pkg::*;
