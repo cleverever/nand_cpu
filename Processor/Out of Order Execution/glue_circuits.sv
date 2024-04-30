c194e82aa5d28ac1c388a6907b95f7362e8d10f8
cleverever <60687194+cleverever@users.noreply.github.com>
2024-04-29 23:15:32 -0700

    Glue and pipeline implementations

--- Processor/Out of Order Execution/glue_circuits.sv
+++ Processor/Out of Order Execution/glue_circuits.sv
@@ -1,23 +1,24 @@
 `include "nand_cpu.svh"
 
+//DECODE
 module decode_glue
 (
     input logic valid,
-    input logic pc,
-    input logic branch_taken,
+
     decoder_ifc.in decoder_in,
+    input logic pc,
+    branch_predictor_output_ifc.in branch_prediction,
     translation_table_ifc.other tt,
     free_reg_list_ifc.other frl,
-    input logic [$clog2(`ROB_SIZE)-1:0] rob_addr,
-
-    input logic flush,
+    input logic [$clog2(`ROB_LENGTH)-1:0] rob_addr,
 
     decode_hazard.out hazard,
 
     branch_valid_ifc.out bv,
 
     execution_buffer_ifc.out eb_out,
-    branch_buffer_ifc.out bb_out
+    branch_buffer_ifc.out bb_out,
+    memory_buffer_ifc.out mb_out
 );
 
 always_comb begin
@@ -25,14 +26,14 @@ always_comb begin
     tt.v_rt_addr = decoder_in.rt_addr;
     tt.v_rw_addr = decoder_in.rw_addr;
 
-    hazard.mispredict = ~flush & branch_taken & (decoder_in.pipeline != BR);
+    hazard.mispredict = branch_taken & (decoder_in.pipeline != BR);
     hazard.recovery_pc = pc + 1;
 
     bv.valid = valid;
     bv.pc = pc;
     bv.branch = decoder_in.branch | decoder_in.jump;
 
-    eb_out.valid = valid & ~flush;
+    eb_out.valid = decoder_in.pipeline == EX;
     eb_out.rob_addr = rob_addr;
     eb_out.alu_op = decoder_in.alu_op;
     eb_out.immdt = decoder_in.immdt;
@@ -44,9 +45,14 @@ always_comb begin
     eb_out.prev_rw_addr = tt.p_rw_addr;
     eb_out.rs_addr = frl.rs_addr;
     eb_out.prev_rs_addr = tt.p_rs_addr;
+
+    bb_out.valid = decoder_in.pipeline == BR;
+
+    mb_out.valid = decoder_in.pipeline == MEM;
 end
 endmodule
 
+//BRANCH
 module b_read_glue
 (
     branch_buffer_ifc.in bb_in,
@@ -54,7 +60,9 @@ module b_read_glue
     metadata_ifc.out metadata,
     rf_dst_ifc.out rf_dst,
 
-    regfile_br_ifc.br rf_port,
+    regfile_d_read_ifc.read rt,
+    regfile_d_read_ifc.read rs,
+
     branch_outcome_ifc.out branch_outcome
 
     branch_hazard.out hazard,
@@ -86,7 +94,8 @@ always_comb begin
 end
 endmodule
 
-interface rf_dst_ifc;
+//EXECUTE
+interface ex_rf_dst_ifc;
 logic write_dst;
 logic [$clog2(`NUM_D_REG)-1:0] rw_addr;
 logic [$clog2(`NUM_S_REG)-1:0] rs_addr;
@@ -103,40 +112,93 @@ endinterface
 
 module e_read_glue
 (
-    execution_buffer_port.in eb_in,
+    execution_buffer_port.in eb,
+    regfile_d_read_ifc.read ra,
+    regfile_d_read_ifc.read rt,
 
     metadata_ifc.out metadata,
-    rf_dst_ifc.out rf_dst,
-
-    regfile_ex_ifc.ex rf_port,
+    ex_rf_dst_ifc.out rf_dst,
     alu_input_ifc.out alu_input
 );
 
 always_comb begin
-    metadata.valid = eb_in.valid;
+    ra.addr = eb.ra_addr;
+    rt.addr = eb.rt_addr;
+
+    metadata.valid = eb.valid;
     metadata.rob_addr = eb.rob_addr;
 
-    rf_dst.write_dst = (eb_in.alu_op == ALU_EQ) | (eb_in.alu_op == ALU_NE);
-    rf_dst.rw_addr = eb_in.rw_addr;
-    rf_dst.rs_addr = eb_in.rs_addr;
+    rf_dst.write_dst = (eb.alu_op == ALU_EQ) | (eb.alu_op == ALU_NE);
+    rf_dst.rw_addr = eb.rw_addr;
+    rf_dst.rs_addr = eb.rs_addr;
 
-    rf_port.ra_addr = eb_in.ra_addr;
-    rf_port.rt_addr = eb_in.rt_addr;
-
-    alu_input.op0 = rf_port.ra_data;
-    alu_input.op1 = eb_in.use_rt? rf_port.rt_data : eb_in.immdt;
-    alu_input.alu_op = eb_in.alu_op;
+    alu_input.op0 = ra.data;
+    alu_input.op1 = eb.use_rt? rt.data : eb.immdt;
+    alu_input.alu_op = eb.alu_op;
 end
 endmodule
 
-module e_alu_glue
+module e_commit_glue
 (
     input logic [15:0] alu_result,
-
+    ex_rf_dst_ifc.in rf_dst,
 
     regfile_d_write_ifc.write ex_d_write,
     regfile_s_write_ifc.write ex_s_write
 );
 
+always_comb begin
+    ex_d_write.valid = ~rf_dst.write_dst;
+    ex_d_write.data = alu_result;
+    ex_d_write.addr = rw_addr;
 
+    ex_s_write.valid = rf_dst.write_dst;
+    ex_s_write.data = alu_result[0];
+    ex_s_write.addr = rs_addr;
+end
+
+endmodule
+
+//MEMORY
+module m_read_glue
+(
+    memory_buffer_port.in mb,
+    regfile_d_read_ifc.read ra,
+    regfile_d_read_ifc.read rt,
+
+    metadata_ifc.out metadata,
+    d_cache_input_ifc.out cache_input,
+    output logic [$clog2(`NUM_D_REG)-1:0] rw_addr
+);
+
+always_comb begin
+    ra.addr = mb.raw_addr;
+    rt.addr = mb.rt_addr;
+
+    metadata.valid = mb.valid;
+    metadata.rob_addr = mb.rob_addr;
+
+    cache_input.mem_op = mb.mem_op;
+    cache_input.address = rt.data;
+    cache_input.data = ra.data;
+
+    rw_addr = raw_addr;
+end
+endmodule
+
+module m_commit_glue
+(
+    metadata_ifc.out metadata,
+    nand_cpu_pkg::MemOp mem_op,
+    d_cache_output_ifc.in cache_output,
+    input logic [15:0] rw_addr,
+
+    regfile_d_write_ifc.out m_a_d_write
+);
+
+always_comb begin
+    m_a_d_write.valid = metadata.valid & (mem_op == MEM_READ);
+    m_a_d_write.data = cache_output.data;
+    m_a_d_write.addr = rw_addr;
+end
 endmodule
--- Processor/Out of Order Execution/pipeline_registers.sv
+++ Processor/Out of Order Execution/pipeline_registers.sv
@@ -14,20 +14,36 @@ modport out
 );
 endinterface
 
+interface pipeline_ctrl_ifc;
+logic retain;
+logic clear;
+
+modport in
+(
+    input retain, clear
+);
+modport out
+(
+    output retain, clear
+);
+endinterface
+
 module i2d
 (
     input logic clk,
     input logic n_rst,
 
-    input logic i2d_flush,
-    input logic i2d_stall,
+    pipeline_ctrl_ifc.in ctrl,
 
-    input logic branch_taken_in,
-    input logic pc_in,
+    input logic valid_in,
+    input logic [7:0] instr_in,
+    input logic [`PC_SIZE-1:0] pc_in,
+    branch_predictor_output_ifc.in branch_prediction_in,
 
     output logic valid_out,
-    output logic pc_out,
-    output logic branch_taken_out
+    output logic [7:0] instr_out,
+    output logic [`PC_SIZE-1:0] pc_out,
+    branch_predictor_output_ifc.out branch_prediction_out
 );
 
 always_ff @(posedge clk) begin
@@ -35,24 +51,26 @@ always_ff @(posedge clk) begin
         //TODO RESET
     end
     else begin
-        valid_out = ~(i2d_flush | i2d_stall);
-        if(~i2d_stall) begin
+        if(~ctrl.stall) begin
             //TODO in = out;
         end
     end
 end
 endmodule
 
+//BRANCH
+
+//EXECUTE
 module e_r2a
 (
     input logic clk,
     input logic n_rst,
 
+    pipeline_ctrl_ifc.in ctrl,
+
     metadata_ifc.in md_in,
     rf_dst_ifc.in rf_dst_in,
     alu_input_ifc.in alu_input_in,
-
-    valid_rob_range_ifc.in rob_valid_range,
 
     metadata_ifc.out md_out,
     rf_dst_ifc.out rf_dst_out,
@@ -60,16 +78,18 @@ module e_r2a
 );
 
 always_ff @(posedge clk) begin
-    md_out.valid <= md_in.valid & rob_valid_range.check_valid(md_in.rob_addr);
-    md_out.rob_addr <= md_in.rob_addr;
+    if(~ctrl.retain) begin
+        md_out.valid <= md_in.valid & ~ctrl.clear;
+        md_out.rob_addr <= md_in.rob_addr;
 
-    rf_dst_out.write_dst <= rf_dst_in.write_dst;
-    rf_dst_out.rw_addr <= rf_dst_in.rw_addr;
-    rf_dst_out.rs_addr <= rf_dst_in.rs_addr;
+        rf_dst_out.write_dst <= rf_dst_in.write_dst;
+        rf_dst_out.rw_addr <= rf_dst_in.rw_addr;
+        rf_dst_out.rs_addr <= rf_dst_in.rs_addr;
 
-    out.op0 <= in.op0;
-    out.op1 <= in.op1;
-    out.alu_op <= in.alu_op;
+        alu_input_out.op0 <= alu_input_in.op0;
+        alu_input_out.op1 <= alu_input_in.op1;
+        alu_input_out.alu_op <= alu_input_in.alu_op;
+    end
 end
 endmodule
 
@@ -98,5 +118,36 @@ always_ff @(posedge clk) begin
     e_c_s_write.valid <= e_a_s_write.valid;
     e_c_s_write.data <= e_a_s_write.data;
     e_c_s_write.addr <= e_a_s_write.addr;
+end
+endmodule
+
+//MEMORY
+module m_r2a
+(
+    input logic clk,
+    input logic n_rst,
+
+    pipeline_ctrl_ifc.in ctrl,
+
+    metadata_ifc.in md_in,
+    d_cache_input_ifc.in cache_input_in,
+    input logic [15:0] rw_addr_in,
+
+    metadata_ifc.out md_out,
+    d_cache_input_ifc.out cache_input_out,
+    output logic rw_addr_out
+);
+
+always_ff @(posedge clk) begin
+    if(~ctrl.retain) begin
+        md_out.valid <= md_in.valid & ~ctrl.clear;
+        md_out.rob_addr <= md_in.rob_addr;
+
+        cache_input_out.address <= cache_input_in.address;
+        cache_input_out.mem_op <= cache_input_in.mem_op;
+        cache_input_out.data <= cache_input_in.data;
+
+        rw_addr_out <= rw_addr_in;
+    end
 end
 endmodule
